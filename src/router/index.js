704aa519d9afee4c6583d8bfc59940da34d93367
User <user@example.com>
2026-07-01 01:54:39 +0800

    Fix router for GitHub Pages with hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import CulturalExchange from '../views/CulturalExchange.vue'
 import CulturalVisa from '../views/CulturalVisa.vue'
@@ -46,7 +46,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/global-youth-exchange/'),
   routes
 })
 
